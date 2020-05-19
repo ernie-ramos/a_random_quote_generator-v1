711f276a9399c3e4b5cb8611989e099256274d8f
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-05-19 15:56:16 -0400

    BG color changes with every click of the button
    Cleaned up the variable names as well.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,15 @@ BY: ERNESTO RAMOS
 @ERNIE_RAMOS_
 ERNRAMOS1991@GMAIL.COM
 ******************************************/
-
+let randomNumber;
+let randomQuote;
+let color;
+let html = "";
+let bgColor = "";
+let pTagQuote = "";
+let pTagSource = "";
+let pTag = "";
+let quote = "";
 
 /***
  * DATA STORED INSIDE DATA.JS
@@ -21,25 +29,40 @@ ERNRAMOS1991@GMAIL.COM
  * @return {string} Returns the index value called in randomQuote.
  */
 function getRandomQuote(quotes) {
-  const randomNumber = Math.floor(Math.random() * quotes.length);
-  const randomQuote = quotes[randomNumber];
+  randomNumber = Math.floor(Math.random() * quotes.length);
+  randomQuote = quotes[randomNumber];
   //console.log(randomQuote)
   return randomQuote;
 }
 
+// @return {number} Returns a random number between 0 and 256.
+function randomRGB() {
+  return Math.floor(Math.random() * 256)
+}
+
+// @return {string} Returns 'rbg(xxx, xxx, xxx)'
+function randomColor() {
+  color = 'rgb(';
+  color += `${randomRGB()}, `;
+  color += `${randomRGB()}, `;
+  color += `${randomRGB()})`;
+  return color;
+}
+
 /**
  * Creates the necessary HTML string to change the quote on the screen.
+ * Background color changes after every click of the button.
  *
  * @var {function} html - Calls on getRandomQuote to pick a random quote from `quotes`.
- * @var {string} pTagX - These two strings create the HTML needed to change the website.
+ * @var {string} pTagX - These three strings create the HTML needed to change the website.
  * @if {var} citation/year - Adds a <span> if citation and/or year is present. Else, closes with </p>.
  * @var {string} quote - Concatenates pTagQuote and pTagSource before it is rendered.
  */
 function printQuote() {
-  let html = getRandomQuote(quotes);
-  let pTagQuote = `<p class="quote">${html.quote}</p>`
-  let pTagSource = `<p class="source">${html.source}`
-  let pTag =  `<p class="tag">${html.tag}</p>`
+  html = getRandomQuote(quotes);
+  pTagQuote = `<p class="quote">${html.quote}</p>`
+  pTagSource = `<p class="source">${html.source}`
+  pTag =  `<p class="tag">${html.tag}</p>`
   if (html.tag) {
     pTag =  `<p class="tag">${html.tag}</p>`
   }
@@ -49,8 +72,9 @@ function printQuote() {
   if (html.year) {
     pTagSource += `<span class="year">${html.year}</span></p>`
   } else { pTagSource += "</p>" }
-  let quote = pTag + pTagQuote + pTagSource;
+  quote = bgColor + pTag + pTagQuote + pTagSource;
   document.getElementById('quote-box').innerHTML = quote;
+  document.body.style.backgroundColor = randomColor();
 }
 
 /***

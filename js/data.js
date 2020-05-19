5c1857a9b41e0c09dfa6a6ce2ddc37ec34626c9a
ernie-ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-05-19 13:43:32 -0400

    Initial commit

---
+++ js/data.js
@@ -0,0 +1,27 @@
+let quotes = [
+  {
+    quote: "I must study politics and war, that our sons may have liberty to study mathematics and philosophy. Our sons ought to study mathematics and philosophy, geography, natural history and naval architecture, navigation, commerce and agriculture in order to give their children a right to study painting, poetry, music, architecture, statuary, tapestry and porcelain.",
+    source: "John Adams",
+    citation: "Letters of John Adams, Addressed to His Wife",
+    year: 1841
+  },
+  {
+    quote: "A republic . . . if you can keep it.",
+    source: "Benjamin Franklin",
+    citation: "The notes of Dr. James McHenry",
+    year: 1787
+  },
+  {
+    quote: "Always do sober what you said you'd do drunk. That will teach you to keep your mouth shut.",
+    source: "Ernest Hemingway"
+  },
+  {
+    quote: "One may be humble out of pride.",
+    source: "Michel de Montaigne",
+    year: "1533 - 1592"
+  },
+  {
+    quote: "The rumor that I'm building a spaceship to get back to my home planet Mars is totally untrue.",
+    source: "Elon Musk"
+  },
+];
---
+++ js/script.js
@@ -0,0 +1,57 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+BY: ERNESTO RAMOS
+5/19/2020
+@ERNIE_RAMOS_
+ERNRAMOS1991@GMAIL.COM
+******************************************/
+
+
+/***
+ * DATA STORED INSIDE DATA.JS
+***/
+
+/**
+ * Returns a random quote from the `quotes` array.
+ *
+ * @param {arr} quotes - Array of objects from data.js.
+ * @var {number} randomNumber - Random value from 0 to length of `quotes`.
+ * @var {string} randomQuote - Uses randomNumber as an index value.
+ * @return {string} Returns the index value called in randomQuote.
+ */
+function getRandomQuote(quotes) {
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomNumber];
+  //console.log(randomQuote)
+  return randomQuote;
+}
+
+/**
+ * Creates the necessary HTML string to change the quote on the screen.
+ *
+ * @var {function} html - Calls on getRandomQuote to pick a random quote from `quotes`.
+ * @var {string} pTagX - These two strings create the HTML needed to change the website.
+ * @if {var} citation/year - Adds a <span> if citation and/or year is present. Else, closes with </p>.
+ * @var {string} quote - Concatenates pTagQuote and pTagSource before it is rendered.
+ */
+function printQuote() {
+  let html = getRandomQuote(quotes);
+  let pTagQuote = `<p class="quote">${html.quote}</p>`
+  let pTagSource = `<p class="source">${html.source}`
+  if (html.citation) {
+    pTagSource +=  `<span class="citation">${html.citation}</span>`
+  }
+  if (html.year) {
+    pTagSource += `<span class="year">${html.year}</span></p>`
+  } else { pTagSource += "</p>" }
+  let quote = pTagQuote + pTagSource;
+  document.getElementById('quote-box').innerHTML = quote;
+}
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
